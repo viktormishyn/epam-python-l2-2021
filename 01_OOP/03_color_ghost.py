26206375aa8cff1112bed566aa8f85ac5c37e37d
Viktor Mishyn <viktormishyn@gmail.com>
2021-10-14 12:27:21 +0300

    WEEK_1 Change `colors` from class to instance variable, add unit test

--- 01_OOP/03_color_ghost.py
+++ 01_OOP/03_color_ghost.py
@@ -1,15 +1,27 @@
 # https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/python
 
 import random
+import unittest
 
 
-class Ghost(object):
-
-    colors = ["white", "yellow", "purple", "red"]
+class Ghost:
 
     def __init__(self) -> None:
         """__init__ will set `color` attribute randomly choosing 
-        from the list of colors (class variable)
+        from set of colors
         """
-        cls = self.__class__
-        self.color = random.choice(cls.colors)
+        self.colors = ["white", "yellow", "purple", "red"]
+        self.color = random.choice(self.colors)
+
+
+class TestGhost(unittest.TestCase):
+
+    def setUp(self):
+        self.colors = ["white", "yellow", "purple", "red"]
+
+    def test_init(self):
+        self.assertIn(Ghost().color, self.colors)
+
+
+if __name__ == "__main__":
+    unittest.main()
